a936c00345f142c17ef963a9be1180809cf0823e
mariocapitbrok <mario@capitbrok.com>
2023-11-23 22:26:29 -0600

    Templates

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-course',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './course.component.html',
+  styleUrl: './course.component.css'
+})
+export class CourseComponent {
+
+}
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,6 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   standalone: true,
-  template: '<h2>Courses</h2>',
+  template: '<h2>{{ "Title: " + getTitle() }}</h2>',
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  title = 'List of courses';
+
+  getTitle() {
+    return this.title;
+  }
+}
