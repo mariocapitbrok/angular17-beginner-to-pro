f29c53be717088bda6bba4f756b040c01dd1877c
mariocapitbrok <mario@capitbrok.com>
2023-11-25 12:29:07 -0600

    Attribute Binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -8,9 +9,15 @@ import { CommonModule } from '@angular/common';
   template: `
     <h2>{{ title }}</h2>
     <img [src]="imageUrl" alt="" />
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
   `,
 })
 export class CoursesComponent {
   title = 'List of courses';
   imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
 }
