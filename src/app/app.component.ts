562d8d80f5c3e51e27b5290beb1fc9095c38adbe
mariocapitbrok <mario@capitbrok.com>
2023-11-27 13:21:02 -0600

    Input properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { FavoriteComponent } from './favorite/favorite.component';
-import { TitleCasePipe } from './title-case.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    FormsModule,
-    FavoriteComponent,
-    TitleCasePipe,
-  ],
+  imports: [CommonModule, RouterOutlet, FavoriteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title!: string;
+  post = {
+    title: 'Title',
+    isFavorite: true,
+  };
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,9 +9,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './favorite.component.css',
 })
 export class FavoriteComponent {
-  isFavorite = true;
+  @Input('isFavorite') isSelected = true;
 
   onClick() {
-    this.isFavorite = !this.isFavorite;
+    this.isSelected = !this.isSelected;
   }
 }
