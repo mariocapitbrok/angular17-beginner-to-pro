4b03ef000719af286cb24076af6da57d49efe044
mariocapitbrok <mario@capitbrok.com>
2023-11-27 12:57:47 -0600

    titleCase custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FavoriteComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    FavoriteComponent,
+    TitleCasePipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hello-world';
+  title!: string;
 }
