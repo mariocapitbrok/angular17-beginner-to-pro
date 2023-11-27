d98509b7410248f9e7c6f4a3c3351937b53f521f
mariocapitbrok <mario@capitbrok.com>
2023-11-27 14:38:24 -0600

    Create a new panel component

---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bootstrap-panel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './panel.component.html',
+  styleUrl: './panel.component.css',
+})
+export class PanelComponent {}
