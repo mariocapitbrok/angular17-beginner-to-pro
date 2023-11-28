0b8df94b0f16f47397c7a57ba818a4374eecfbb2
mariocapitbrok <mario@capitbrok.com>
2023-11-28 13:58:41 -0600

    Updating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -32,4 +32,18 @@ export class PostsComponent {
         console.log(response);
       });
   }
+
+  updatePost(post: Post) {
+    //this.http.put(this.url, JSON.stringify(post));
+    this.http
+      .patch(
+        this.url + '/' + post.id,
+        JSON.stringify({
+          isRead: true,
+        })
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
