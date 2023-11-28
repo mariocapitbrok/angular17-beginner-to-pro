bf1a666438bc465b9621f7bec5040da45e0cac70
mariocapitbrok <mario@capitbrok.com>
2023-11-28 11:37:38 -0600

    Change Password Form Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormBuilderComponent],
+  imports: [CommonModule, RouterOutlet, ChangePasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/change-password/change-password.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { PasswordValidators } from './password.validators';
+
+@Component({
+  selector: 'change-password',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './change-password.component.html',
+  styleUrl: './change-password.component.css',
+})
+export class ChangePasswordComponent {
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group(
+      {
+        oldPassword: [
+          '',
+          Validators.required,
+          PasswordValidators.validOldPassword,
+        ],
+        newPassword: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      {
+        validator: PasswordValidators.passwordsShouldMatch,
+      }
+    );
+  }
+
+  get oldPassword() {
+    return this.form.get('oldPassword');
+  }
+  get newPassword() {
+    return this.form.get('newPassword');
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+}
