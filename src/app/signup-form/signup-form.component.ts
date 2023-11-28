3715fa79d0b63c9d1c98aa52572b4a8988e83583
mariocapitbrok <mario@capitbrok.com>
2023-11-28 07:27:14 -0600

    Signup Reactive Form

---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { UsernameValidators } from './username.validators';
+import { AuthorsService } from '../authors.service';
+
+@Component({
+  selector: 'signup-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.css'],
+})
+export class SignupFormComponent {
+  form = new FormGroup({
+    account: new FormGroup({
+      username: new FormControl(
+        '',
+        Validators.required,
+        UsernameValidators.shouldBeUnique
+      ),
+      password: new FormControl('', Validators.required),
+    }),
+  });
+
+  get username() {
+    return this.form.get('account.username');
+  }
+
+  login() {
+    //let isValid = authService.login(this.form.value);
+    //if (!isValid) {
+    this.form.setErrors({
+      invalidLogin: true,
+    });
+    // }
+  }
+}
